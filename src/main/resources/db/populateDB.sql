80a388c3cb9defd75c0f2da31c5e3c97579ea7fe
Anatoliy_Glushkov <glushkov1990@gmail.com>
2023-10-22 17:34:27 +0300

    HW3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2023-10-30 10:00:00', 'Завтрак юзер', 500),
+       (100000, '2023-10-30 13:00:00', 'Обед юзер', 1000),
+       (100000, '2023-10-30 20:00:00', 'Ужин юзер', 500),
+       (100000, '2023-10-31 00:00:00', 'Еда на граничное значение юзер', 100),
+       (100000, '2023-10-31 10:00:00', 'Завтрак юзер', 1000),
+       (100000, '2023-10-31 13:00:00', 'Обед юзер', 500),
+       (100000, '2023-10-31 20:00:00', 'Ужин юзер', 410),
+       (100001, '2023-10-30 10:00:00', 'Завтрак админ', 500),
+       (100001, '2023-10-30 12:00:00', 'ужин админ', 700);
